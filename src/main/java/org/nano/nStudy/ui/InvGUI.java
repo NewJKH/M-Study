97b9ab43919f83a453027509c8eb8dec8ef69c81
NewJKH <newsun0805@gmail.com>
2025-04-19 12:01:50 +0900

    update gui

--- src/main/java/org/nano/nStudy/ui/InvGUI.java
+++ src/main/java/org/nano/nStudy/ui/InvGUI.java
@@ -4,12 +4,20 @@ import org.bukkit.Bukkit;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.jetbrains.annotations.NotNull;
+import org.nano.nStudy.repository.ItemRepository;
 
 public class InvGUI implements InventoryHolder {
     private Inventory inventory;
+    private final ItemRepository repository;
 
-    public InvGUI() {
-        inventory = Bukkit.createInventory(this,54,"이걸로 테스트");
+    public InvGUI(ItemRepository itemRepository) {
+        this.repository = itemRepository;
+        this.inventory = Bukkit.createInventory(this,54,"이걸로 테스트");
+    }
+
+    public void setup(){
+        repository.getContainer()
+                .forEach(itemStack -> inventory.addItem(itemStack));
     }
 
 
