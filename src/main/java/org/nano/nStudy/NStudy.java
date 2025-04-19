b0fc71f3a8efc37ecccdaa123fb559d29a8f10e3
NewJKH <newsun0805@gmail.com>
2025-04-19 12:07:18 +0900

    update command

--- src/main/java/org/nano/nStudy/NStudy.java
+++ src/main/java/org/nano/nStudy/NStudy.java
@@ -1,13 +1,18 @@
 package org.nano.nStudy;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.nano.nStudy.app.Command;
+import org.nano.nStudy.repository.ItemRepository;
+
+import java.util.Objects;
 
 public final class NStudy extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        ItemRepository itemRepository = new ItemRepository();
 
-
+        Objects.requireNonNull(getCommand("창고")).setExecutor(new Command(itemRepository));
     }
 
     @Override
--- src/main/java/org/nano/nStudy/app/Command.java
+++ src/main/java/org/nano/nStudy/app/Command.java
@@ -1,24 +1,26 @@
 package org.nano.nStudy.app;
 
-import org.bukkit.Material;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import org.nano.nStudy.util.ItemBuilder;
-
-import java.util.List;
+import org.nano.nStudy.repository.ItemRepository;
+import org.nano.nStudy.ui.InvGUI;
 
 public class Command implements CommandExecutor {
+    private final ItemRepository repository;
+
+    public Command(ItemRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, org.bukkit.command.@NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        ItemStack item = ItemBuilder.builder()
-                .create(Material.IRON_SWORD)
-                .material(Material.DIAMOND_SWORD)
-                .display("§b강력한 검")
-                .lore(List.of("§7이 검은...", "§c강력한 힘을 가지고 있다."))
-                .model(1001)
-                .build();
+        if ( commandSender instanceof Player player){
+            InvGUI gui = new InvGUI(repository);
+            gui.setup();
+            player.openInventory(gui.getInventory());
+        }
         return false;
     }
 }
